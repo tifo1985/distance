3520c2c99635a9818c52e5b64a346646dda8d31e
aellouze <abdellatif.ellouze@gmail.com>
2019-09-22 01:08:47 +0200

    API install dependencies

--- distance-api/config/bundles.php
+++ distance-api/config/bundles.php
@@ -3,4 +3,5 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
 ];
